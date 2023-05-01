8333f38658ce07d9e26df96e2c9bf17af48b07f4
Robin <24991308+Rolami@users.noreply.github.com>
2023-05-01 14:44:59 +0200

    Final commit of the guide. It's now functional and up and running.

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -5,34 +5,36 @@ using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
-    public class PizzaModel : PageModel
-    {
+public class PizzaModel : PageModel
+{
+    public List<Pizza> pizzas = new();
+    [BindProperty]
+    public Pizza NewPizza { get; set; } = new();
+
     public void OnGet()
     {
-
         pizzas = PizzaService.GetAll();
-    
     }
-
-    public List<PizzaModel> pizzas = new();
-
+    
     public string GlutenFreeText(Pizza pizza)
     {
-        return pizza.IsGlutenFree ? "Gluten Free" : "Not Gluten Free";
+        return pizza.IsGlutenFree ? "Gluten Free": "Not Gluten Free";
     }
 
     public IActionResult OnPost()
     {
-    if (!ModelState.IsValid)
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        PizzaService.Add(NewPizza);
+        return RedirectToAction("Get");
+    }
+
+    public IActionResult OnPostDelete(int id)
     {
-        return Page();
+        PizzaService.Delete(id);
+        return RedirectToAction("Get");
     }
-    PizzaService.Add(NewPizza);
-    return RedirectToAction("Get");
-    }
-
-    [BindProperty]
-    public Pizza NewPizza { get; set; } = new();
-
-    }
+}
 }
